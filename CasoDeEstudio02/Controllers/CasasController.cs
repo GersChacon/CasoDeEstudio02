6a2834c5323985a5ffbf2398b98b0612290a665c
Alex M <145505088+ALE20201@users.noreply.github.com>
2025-08-14 15:22:21 -0600

    Caso de estudio02 parte 3

--- CasoDeEstudio02/Controllers/CasasController.cs
+++ CasoDeEstudio02/Controllers/CasasController.cs
@@ -5,91 +5,118 @@ using System.Linq;
 using System.Web.Mvc;
 using System.Collections.Generic; 
 using System.ComponentModel.DataAnnotations; 
-using System.Web;
+using System.Web; 
 
-namespace CasoEstudio2.Controllers
+namespace CasoDeEstudio02.Controllers
 {
     public class CasasController : Controller
     {
-        private readonly CasoEstudioKNEntities db = new CasoEstudioKNEntities();
-
-        // GET: Casas/Alquilar
-        public ActionResult Alquilar()
-        {
-            var model = new CasasModel
-            {
-                CasasDisponibles = new SelectList(
-                    db.CasasSistema.Where(c => c.UsuarioAlquiler == null).ToList(),
-                    "IdCasa", "DescripcionCasa")
-            };
-            return View(model);
-        }
-
-        // POST: Casas/Alquilar
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Alquilar(CasasModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    var casa = db.CasasSistema.Find(model.IdCasa);
-                    if (casa == null)
-                    {
-                        ModelState.AddModelError("", "La casa seleccionada no existe.");
-                    }
-                    else if (casa.UsuarioAlquiler != null)
-                    {
-                        ModelState.AddModelError("", "La casa seleccionada ya está alquilada.");
-                    }
-                    else
-                    {
-                        casa.UsuarioAlquiler = model.UsuarioAlquiler;
-                        casa.FechaAlquiler = DateTime.Now;
-                        db.SaveChanges();
-                        return RedirectToAction("Consulta");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    ModelState.AddModelError("", "Ocurrió un error al procesar el alquiler: " + ex.Message);
-                }
-            }
-
-            // If we got this far, something failed; redisplay form
-            model.CasasDisponibles = new SelectList(
-                db.CasasSistema.Where(c => c.UsuarioAlquiler == null).ToList(),
-                "IdCasa", "DescripcionCasa");
-            return View(model);
-        }
-
-        // AJAX method to get house price
-        public JsonResult ObtenerPrecioCasa(long id)
-        {
-            var precio = db.CasasSistema
-                           .Where(c => c.IdCasa == id)
-                           .Select(c => c.PrecioCasa)
-                           .FirstOrDefault();
-            return Json(precio, JsonRequestBehavior.AllowGet);
-        }
-
-        // GET: Casas/Consulta
+        [HttpGet]
         public ActionResult Consulta()
         {
-            var casas = db.CasasSistema
-                .Where(c => c.PrecioCasa >= 115000 && c.PrecioCasa <= 180000)
-                .OrderBy(c => c.UsuarioAlquiler != null) // Available houses first
-                .Select(c => new CasasConsultaModel
-                {
-                    DescripcionCasa = c.DescripcionCasa,
-                    PrecioCasa = c.PrecioCasa,
-                    UsuarioAlquiler = c.UsuarioAlquiler,
-                    Estado = c.UsuarioAlquiler == null ? "Disponible" : "Reservada",
-                    FechaAlquiler = c.FechaAlquiler.HasValue ? c.FechaAlquiler.Value.ToString("dd/MM/yyyy") : ""
-                }).ToList();
+            using (var dbContext = new CasoEstudioKNEntities())
+            {
+                var lista = dbContext.CasasSistema
+                    .Where(c => c.PrecioCasa >= 115000 && c.PrecioCasa <= 180000)
+                    .Select(c => new CasasModel
+                    {
+                        IdCasa = c.IdCasa,
+                        DescripcionCasa = c.DescripcionCasa,
+                        PrecioCasa = c.PrecioCasa,
+                        UsuarioAlquiler = c.UsuarioAlquiler,
+                        FechaAlquiler = c.FechaAlquiler
+                    })
+                    .ToList();
 
-            return View(casas);
+                return View(lista);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Alquiler()
+        {
+            using (var dbContext = new CasoEstudioKNEntities())
+            {
+                var disponibles = dbContext.CasasSistema
+                    .Where(c => c.UsuarioAlquiler == null && c.FechaAlquiler == null)
+                    .OrderBy(c => c.DescripcionCasa)
+                    .ToList();
+
+                var model = new AlquilarCasaViewModel
+                {
+                    CasasDisponibles = disponibles.Select(c => new SelectListItem
+                    {
+                        Value = c.IdCasa.ToString(),
+                        Text = c.DescripcionCasa
+                    }).ToList()
+                };
+
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public JsonResult PrecioDeCasa(long id)
+        {
+            using (var dbContext = new CasoEstudioKNEntities())
+            {
+                var precio = dbContext.CasasSistema
+                    .Where(c => c.IdCasa == id)
+                    .Select(c => c.PrecioCasa)
+                    .FirstOrDefault();
+
+                return Json(precio, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Alquiler(AlquilarCasaViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                using (var dbContext = new CasoEstudioKNEntities())
+                {
+                    model.CasasDisponibles = dbContext.CasasSistema
+                        .Where(c => c.UsuarioAlquiler == null && c.FechaAlquiler == null)
+                        .OrderBy(c => c.DescripcionCasa)
+                        .Select(c => new SelectListItem
+                        {
+                            Value = c.IdCasa.ToString(),
+                            Text = c.DescripcionCasa
+                        }).ToList();
+                }
+
+                return View(model);
+            }
+
+            using (var dbContext = new CasoEstudioKNEntities())
+            {
+                var casa = dbContext.CasasSistema
+                    .SingleOrDefault(c => c.IdCasa == model.IdCasa && c.UsuarioAlquiler == null && c.FechaAlquiler == null);
+
+                if (casa == null)
+                {
+                    ModelState.AddModelError("", "La casa seleccionada ya no está disponible.");
+                    model.CasasDisponibles = dbContext.CasasSistema
+                        .Where(c => c.UsuarioAlquiler == null && c.FechaAlquiler == null)
+                        .OrderBy(c => c.DescripcionCasa)
+                        .Select(c => new SelectListItem
+                        {
+                            Value = c.IdCasa.ToString(),
+                            Text = c.DescripcionCasa
+                        }).ToList();
+
+                    return View(model);
+                }
+
+                casa.UsuarioAlquiler = model.UsuarioAlquiler;
+                casa.FechaAlquiler = DateTime.Now;
+
+                dbContext.SaveChanges();
+            }
+
+            return RedirectToAction("Consulta");
         }
     }
 }
