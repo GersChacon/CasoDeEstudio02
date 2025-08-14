7388e99242888fb13d3bd998ead9ee2866685927
Alex M <145505088+ALE20201@users.noreply.github.com>
2025-08-14 15:22:21 -0600

    Caso de estudio02 parte 3

--- CasoDeEstudio02/Controllers/CasasController.cs
+++ CasoDeEstudio02/Controllers/CasasController.cs
@@ -3,6 +3,9 @@ using CasoDeEstudio02.Models;
 using System;
 using System.Linq;
 using System.Web.Mvc;
+using System.Collections.Generic; 
+using System.ComponentModel.DataAnnotations; 
+using System.Web; 
 
 namespace CasoDeEstudio02.Controllers
 {
@@ -32,7 +35,89 @@ namespace CasoDeEstudio02.Controllers
         [HttpGet]
         public ActionResult Alquiler()
         {
-            return View();
+            using (var dbContext = new CasoEstudioKNEntities())
+            {
+                var disponibles = dbContext.CasasSistema
+                    .Where(c => c.UsuarioAlquiler == null && c.FechaAlquiler == null)
+                    .OrderBy(c => c.DescripcionCasa)
+                    .ToList();
+
+                var model = new AlquilarCasaViewModel
+                {
+                    CasasDisponibles = disponibles.Select(c => new SelectListItem
+                    {
+                        Value = c.IdCasa.ToString(),
+                        Text = c.DescripcionCasa
+                    }).ToList()
+                };
+
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public JsonResult PrecioDeCasa(long id)
+        {
+            using (var dbContext = new CasoEstudioKNEntities())
+            {
+                var precio = dbContext.CasasSistema
+                    .Where(c => c.IdCasa == id)
+                    .Select(c => c.PrecioCasa)
+                    .FirstOrDefault();
+
+                return Json(precio, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Alquiler(AlquilarCasaViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                using (var dbContext = new CasoEstudioKNEntities())
+                {
+                    model.CasasDisponibles = dbContext.CasasSistema
+                        .Where(c => c.UsuarioAlquiler == null && c.FechaAlquiler == null)
+                        .OrderBy(c => c.DescripcionCasa)
+                        .Select(c => new SelectListItem
+                        {
+                            Value = c.IdCasa.ToString(),
+                            Text = c.DescripcionCasa
+                        }).ToList();
+                }
+
+                return View(model);
+            }
+
+            using (var dbContext = new CasoEstudioKNEntities())
+            {
+                var casa = dbContext.CasasSistema
+                    .SingleOrDefault(c => c.IdCasa == model.IdCasa && c.UsuarioAlquiler == null && c.FechaAlquiler == null);
+
+                if (casa == null)
+                {
+                    ModelState.AddModelError("", "La casa seleccionada ya no está disponible.");
+                    model.CasasDisponibles = dbContext.CasasSistema
+                        .Where(c => c.UsuarioAlquiler == null && c.FechaAlquiler == null)
+                        .OrderBy(c => c.DescripcionCasa)
+                        .Select(c => new SelectListItem
+                        {
+                            Value = c.IdCasa.ToString(),
+                            Text = c.DescripcionCasa
+                        }).ToList();
+
+                    return View(model);
+                }
+
+                casa.UsuarioAlquiler = model.UsuarioAlquiler;
+                casa.FechaAlquiler = DateTime.Now;
+
+                dbContext.SaveChanges();
+            }
+
+            return RedirectToAction("Consulta");
         }
     }
 }
+
---
+++ CasoDeEstudio02/Models/AlquilerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc; 
+
+namespace CasoDeEstudio02.Models
+{
+    public class AlquilarCasaViewModel
+    {
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Debe seleccionar una casa.")]
+        public long IdCasa { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Display(Name = "Precio mensual")]
+        public decimal PrecioCasa { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "El usuario de alquiler es obligatorio.")]
+        [System.ComponentModel.DataAnnotations.Display(Name = "Usuario que alquila")]
+        public string UsuarioAlquiler { get; set; }
+
+        public List<SelectListItem> CasasDisponibles { get; set; } = new List<SelectListItem>();
+    }
+}
