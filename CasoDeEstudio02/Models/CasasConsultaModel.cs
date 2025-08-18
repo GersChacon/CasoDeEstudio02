fa6ffac3ffa48f28f589044de4ca2cc762d22743
Alex M <145505088+ALE20201@users.noreply.github.com>
2025-08-18 17:21:30 -0600

    en teoria bien

---
+++ CasoDeEstudio02/Models/CasasConsultaModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CasoDeEstudio02.Models
+{
+    public class CasasModel
+    {
+        public long IdCasa { get; set; }
+        public string DescripcionCasa { get; set; }
+        public decimal PrecioCasa { get; set; }
+        public string UsuarioAlquiler { get; set; }
+        public DateTime? FechaAlquiler { get; set; }
+
+    }
+}
